9e9bfccd992120c96deff4f592353f425e66186f
AntiSol <antisol@antisol.org>
2016-08-23 16:30:11 +1000

    github #258: moar animation (menus slide down to make it obvious the rotating icons are part of the animation)

--- javascript/GroupedCmsMenu.js
+++ javascript/GroupedCmsMenu.js
@@ -1,11 +1,11 @@
 (function($) {
 	$("#MainMenu li.children").hover(
 		function() { 
-			$(this).addClass("active").find("ul").show(); 
+			$(this).addClass("active").find("ul").slideDown(333); 
 			//$(this).find('i.fa').removeClass('fa-caret-down').addClass('fa-caret-up');
 		},
 		function() { 
-			$(this).removeClass("active").find("ul").hide();
+			$(this).removeClass("active").find("ul").slideUp(333);
 			//$(this).find('i.fa').removeClass('fa-caret-up').addClass('fa-caret-down');
 		}
 	);
